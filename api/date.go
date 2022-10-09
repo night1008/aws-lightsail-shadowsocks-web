616ee2af1442631d4ffadb8c3a1c9c13115bb14a
night1008 <1179039379@qq.com>
2022-10-09 20:27:32 +0800

    add oss api

--- api/date.go
+++ api/date.go
@@ -1,12 +1,58 @@
 package handler
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
-	"time"
+	"os"
+	"strings"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintf(w, currentTime)
+	endpoint := os.Getenv("OSS_ENDPOINT")
+	accessKeyID := os.Getenv("OSS_ACCESS_KEY_ID")
+	accessKeySecret := os.Getenv("OSS_ACCESS_KEY_SECRET")
+	bucketName := os.Getenv("OSS_BUCKET")
+
+	client, err := oss.New(endpoint, accessKeyID, accessKeySecret)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	bucket, err := client.Bucket(bucketName)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	objectsResult, err := bucket.ListObjects()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	outputPrefix := "outputs/"
+	for _, object := range objectsResult.Objects {
+		if !strings.HasPrefix(object.Key, outputPrefix) {
+			continue
+		}
+		body, err := bucket.GetObject(object.Key)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+
+		defer body.Close()
+		var buf bytes.Buffer
+		if _, err := io.Copy(&buf, body); err != nil {
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+		fmt.Fprintf(w, string(buf.Bytes()))
+		break
+	}
 }
