b9698c9528fd7268fcc429e5bd79774930cc8b07
night1008 <1179039379@qq.com>
2022-11-14 21:20:56 +0800

    add workflow dispatch

--- api/submit.go
+++ api/submit.go
@@ -1,14 +1,21 @@
 package handler
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
 func SubmitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		fmt.Fprintf(w, "invalid http method")
+		return
+	}
+
 	endpoint := os.Getenv("OSS_ENDPOINT")
 	accessKeyID := os.Getenv("OSS_ACCESS_KEY_ID")
 	accessKeySecret := os.Getenv("OSS_ACCESS_KEY_SECRET")
@@ -31,5 +38,37 @@ func SubmitHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	githubAccessToken := os.Getenv("GITHUB_ACCESS_TOKEN")
+	if err := sendGithubWorkflowDispatchRequest(githubAccessToken); err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
 	fmt.Fprintf(w, `{"success": true}`)
+}
+
+func sendGithubWorkflowDispatchRequest(githubAccessToken string) error {
+	repositoryURL := "https://api.github.com/repos/night1008/aws-lightsail-shadowsocks-tf/dispatches"
+
+	jsonBody := []byte(`{"event_type": "deploy-instances"}`)
+	bodyReader := bytes.NewReader(jsonBody)
+	req, err := http.NewRequest(http.MethodPost, repositoryURL, bodyReader)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Accept", "application/vnd.github+json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", githubAccessToken))
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode == http.StatusNoContent {
+		return nil
+	} else {
+		return fmt.Errorf("github response status code %d", resp.StatusCode)
+	}
 }
