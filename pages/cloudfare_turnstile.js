8fa7a5cef1c3f362f0de037af8d60900168f8089
night1008 <1179039379@qq.com>
2023-10-14 21:50:32 +0800

    add cloudfare turnstile page

---
+++ pages/cloudfare_turnstile.js
@@ -0,0 +1,16 @@
+export default function CloudfareTurnstile() {
+  return (
+    <div className="container">
+      <Head>
+        <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+      </Head>
+
+      <form action="/login" method="POST">
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <div class="cf-turnstile" data-sitekey="0x4AAAAAAALovSRVqJoLSnud"></div>
+        <button type="submit" value="Submit">Log in</button>
+      </form>
+    </div>
+  )
+}
